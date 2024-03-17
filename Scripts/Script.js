79b73e1ddaed2e2c15b8bf0a9103ba04acf54c4b
asmaulhosen45 <asmaulhosen45@gmail.com>
2024-03-17 14:39:59 +0600

    added search form

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,7 +1,21 @@
+// Header -----------
 let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector("nav");
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active')
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active')
 }
