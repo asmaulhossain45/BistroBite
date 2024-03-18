eeb01564d5ffa2348fba274c567c95ef11645cda
asmaulhosen45 <asmaulhosen45@gmail.com>
2024-03-18 11:58:21 +0600

    added reviews section

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -33,3 +33,27 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true
   });
+
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+        0: {
+            slidesPerView:1,
+        },
+        640: {
+            slidesPerView:2,
+        },
+        768: {
+            slidesPerView:2,
+        },
+        1024: {
+            slidesPerView:3,
+        }
+    }
+  });
