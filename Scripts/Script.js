b8032bd42d0cc7acc522ab747f7b9c0e2037d6a9
asmaulhosen45 <asmaulhosen45@gmail.com>
2024-03-18 15:44:23 +0600

    added footer

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -3,57 +3,90 @@ let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector("nav");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
 
-document.querySelector('#search-icon').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active')
-}
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let hight = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute("id");
 
-document.querySelector('#close').onclick = () => {
-    document.querySelector('#search-form').classList.remove('active')
-}
-
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true
-  });
-
-  var swiper = new Swiper(".reviews-slider", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-    loop:true,
-    breakpoints: {
-        0: {
-            slidesPerView:1,
-        },
-        640: {
-            slidesPerView:2,
-        },
-        768: {
-            slidesPerView:2,
-        },
-        1024: {
-            slidesPerView:3,
-        }
+    if (top >= offset && top < offset + hight) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
     }
   });
+};
+
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
+
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+document
+  .getElementById("copyright")
+  .appendChild(document.createTextNode(new Date().getFullYear()));
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 1000);
+}
+
+window.onload = fadeOut;
